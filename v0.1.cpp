071da5050dd08bdf5ef6deef380d9173072c2e49
MantasMaurius1 <48581823+MantasMaurius1@users.noreply.github.com>
2019-03-20 16:47:51 +0200

    Update v0.1.cpp

--- v0.1.cpp
+++ v0.1.cpp
@@ -7,8 +7,8 @@ using namespace std;
 struct studentas
 {
     char vardas[20], pavarde[20];
-    int nd, egz, N;
-    double galut=5;
+    int nd, egz;
+    double galut;
 };
 int main()
 {
@@ -25,10 +25,11 @@ cout<<"Iveskite namu darbu rezultata"<<endl;
 cin>>(r+i)->nd;
 cout<<"Iveskite studento egzamino rezultata"<<endl;
 cin>>(r+i)->egz;
+(r+i)->galut=(0.4*(r+i)->nd)+(0.6*(r+i)->egz);
 }
+
 cout<<left<<setw(10)<<"Vardas"<<setw(10)<<"Pavarde"<<setw(20)<<"Galutinis (Vid.)"<<endl;
 cout<<"-"<<setfill('-')<<setw(35)<<"-"<<endl;
-
 for(int i=0;i<3;i++)
 {
 cout<<left<<setfill(' ')<<setw(10)<<(r+i)->vardas<<setfill(' ')<<setw(10)<<(r+i)->pavarde<<setfill(' ')<<setw(15)<<right<<fixed<<setprecision(2)<<(r+i)->galut<<endl;
