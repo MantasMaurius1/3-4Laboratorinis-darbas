1ff6535095108b1ccc4e06d86af4df0047cdbf34
MantasMaurius1 <48581823+MantasMaurius1@users.noreply.github.com>
2019-03-22 10:01:46 +0200

    Update v0.1.cpp

--- v0.1.cpp
+++ v0.1.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <stdio.h>
 #include <iomanip>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+using std::vector;
 
 struct studentas
 {
     char vardas[20], pavarde[20];
-    int egz, nd;
-    double galut, vdrk;
+    int egz;
+    double galutv, vdrk, galutm;
 };
 int main()
 {
 studentas skaicius[3];
 studentas *r;
-studentas ndskaicius[3];
-studentas *t;
-t=ndskaicius;
 r=skaicius;
 for(int i=0;i<3;i++)
 {
@@ -25,21 +25,26 @@ cin>>(r+i)->vardas;
 cout<<"Iveskite studento pavarde"<<endl;
 cin>>(r+i)->pavarde;
 cout<<"Iveskite namu darbu rezultatus"<<endl;
+vector<double> ndm;
+int nd;
 for (int j=0;j<3;j++)
 {
-cin>>(t+j)->nd;
-(r+i)->vdrk=((t+j)->nd)/3;
+cin>>nd;
+ndm.push_back(nd);
+ndm.size();
+sort(ndm.begin(), ndm.end());
 }
 cout<<"Iveskite studento egzamino rezultata"<<endl;
 cin>>(r+i)->egz;
-(r+i)->galut=(0.4*(r+i)->vdrk)+(0.6*(r+i)->egz);
+(r+i)->vdrk=(ndm[0]+ndm[1]+ndm[2])/3;
+(r+i)->galutv=(0.4*(r+i)->vdrk)+(0.6*(r+i)->egz);
+(r+i)->galutm=(0.4*ndm[1])+(0.6*(r+i)->egz);
 }
-
-cout<<left<<setw(10)<<"Vardas"<<setw(10)<<"Pavarde"<<setw(20)<<"Galutinis (Vid.)"<<endl;
-cout<<"-"<<setfill('-')<<setw(35)<<"-"<<endl;
+cout<<left<<setw(10)<<"Vardas"<<setw(10)<<"Pavarde"<<setw(15)<<"Galutinis (Vid.)"<<" / "<<"Galutinis(Med.)"<<endl;
+cout<<"-"<<setfill('-')<<setw(53)<<"-"<<endl;
 for(int i=0;i<3;i++)
 {
-cout<<left<<setfill(' ')<<setw(10)<<(r+i)->vardas<<setfill(' ')<<setw(10)<<(r+i)->pavarde<<setfill(' ')<<setw(15)<<right<<fixed<<setprecision(2)<<(r+i)->galut<<endl;
+cout<<left<<setfill(' ')<<setw(10)<<(r+i)->vardas<<setfill(' ')<<setw(10)<<(r+i)->pavarde<<setfill(' ')<<setw(15)<<right<<fixed<<setprecision(2)<<(r+i)->galutv<<setfill(' ')<<setw(15)<<right<<fixed<<setprecision(2)<<(r+i)->galutm<<endl;
 }
 }
 
