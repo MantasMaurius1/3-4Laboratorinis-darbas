af8ec185c7de4130aa1089dfb84b826b969288be
MantasMaurius1 <48581823+MantasMaurius1@users.noreply.github.com>
2019-04-01 21:36:35 +0300

    Create v0.2.cpp

---
+++ v0.2.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <numeric>
+
+using namespace std;
+using std::vector;
+
+struct studentas
+{
+
+    string vardas, pavarde;
+    double egz;
+    vector<double> ndm, vdrk, galutv, galutm;
+};
+int main()
+{
+double nd; char uzkl='t';
+vector<studentas> lent; studentas temp;
+while (uzkl!='n')
+{
+cout<<"Iveskite studento varda"<<endl;
+cin>>temp.vardas;
+cout<<"Iveskite studento pavarde"<<endl;
+cin>>temp.pavarde; nd=1;
+cout<<"Iveskite namu darbu rezultatus(ivede 0 pereisite prie sekancio etapo)"<<endl;         //Namu darbu ivedimo ciklas
+while(nd!=0)
+{
+cin>>nd;
+if(nd>0&&nd<=10)
+{
+temp.ndm.push_back(nd);
+temp.ndm.size();
+sort(temp.ndm.begin(), temp.ndm.end());
+}
+else if(nd==0){}
+else
+{
+cout<<"Namu darbu rezultatai, tik desimtbaleje sistemoje"<<endl;
+}
+}
+cout<<"Iveskite studento egzamino rezultata"<<endl;   //Egzamino ivedimas
+cin.clear(); cin.ignore(256,'\n');
+cin>>temp.egz;
+if(temp.egz>0&&temp.egz<=10){}
+else
+{
+cout<<"Egzamino rezultatas, tik desimtbaleje sistemoje"<<endl;
+}
+temp.vdrk.push_back((accumulate(temp.ndm.begin() , temp.ndm.end(), 0))/(temp.ndm.size())); //Galutinis su vidurkiu
+temp.galutv.push_back((temp.vdrk.back()*0.4)+(temp.egz*0.6));
+size_t size=temp.ndm.size();                         //Galutinis su mediana
+if (size%2==0)
+{
+temp.galutm.push_back((((temp.ndm[size/2-1]+temp.ndm[size/2])/2)*0.4)+(temp.egz*0.6));
+}
+else
+{
+temp.galutm.push_back(((temp.ndm[size / 2])*0.4)+(temp.egz*0.6));
+}
+lent.push_back(temp);
+temp.ndm.clear();
+temp.vdrk.clear();
+temp.galutv.clear();
+temp.galutm.clear();
+cout<<"Norint baigti ivedima, iveskite n"<<endl;
+cin>>uzkl;
+}
+cout<<left<<setw(10)<<"Vardas"<<setw(10)<<"Pavarde"<<setw(15)<<"Galutinis (Vid.)"<<" / "<<"Galutinis(Med.)"<<endl; // Isvedimas
+cout<<"-"<<setfill('-')<<setw(53)<<"-"<<endl;
+for(auto i: lent)
+{
+for(auto v: i.galutv)
+for(auto m: i.galutm)
+cout<<left<<setfill(' ')<<setw(10)<<i.vardas<<setfill(' ')<<setw(10)<<i.pavarde<<setfill(' ')<<setw(15)<<right<<fixed<<setprecision(2)<<v<<setfill(' ')<<setw(15)<<right<<fixed<<setprecision(2)<<m<<endl;
+}
+return 0;
+}
