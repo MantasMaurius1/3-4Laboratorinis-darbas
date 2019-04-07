f2bb2f908e8c0f370dceecdcb1b108a5ccbd3a9d
MantasMaurius1 <48581823+MantasMaurius1@users.noreply.github.com>
2019-04-07 15:46:46 +0300

    Update v0.2.cpp

--- v0.2.cpp
+++ v0.2.cpp
@@ -7,14 +7,12 @@
 #include <numeric>
 #include <fstream>
 #include <cstdlib>
-#include <sstream>
 
 using namespace std;
 using std::vector;
 
 struct studentas
 {
-
     string vardas, pavarde;
     double egz;
     vector<double> ndm, vdrk, galutv, galutm;
@@ -23,7 +21,7 @@ int main()
 {
 ifstream is;
 is.open("kursiokai.txt");
-double nd; char uzkl='t'; string budas;
+double nd; char uzkl='t'; string budas; char A[9999];
 vector<studentas> lent; studentas temp;
 cout<<"Iveskite programos veikimo buda(rankinis,isfailo,atsitiktinis)"<<endl;
 cin>>budas;
@@ -80,7 +78,7 @@ cout<<"Norint baigti ivedima, iveskite n"<<endl;
 cin>>uzkl;
 }
 }
-else if (budas=="isfailo")                                                                    //Ivedimas is failo
+if (budas=="isfailo")                                                                    //Ivedimas is failo
 {
 if(is.fail())
 {
@@ -89,10 +87,33 @@ exit(-1);
 }
 else
 {
-char str[100];
-while(is.get(*str))
-cout<<str;
-is.close();
+is.ignore(58);
+while(is.get(*A)){
+is>>temp.vardas>>temp.pavarde;
+while(temp.ndm.size()!=5){
+is>>nd;                                                                                  //Namu darbu ivedimo ciklas
+temp.ndm.size();
+temp.ndm.push_back(nd);
+}
+is>>temp.egz;
+sort(temp.ndm.begin(), temp.ndm.end());
+temp.vdrk.push_back((accumulate(temp.ndm.begin() , temp.ndm.end(), 0))/(temp.ndm.size()));  //Galutinis su vidurkiu
+temp.galutv.push_back((temp.vdrk.back()*0.4)+(temp.egz*0.6));
+size_t size=temp.ndm.size();                                                                //Galutinis su mediana
+if (size%2==0)
+{
+temp.galutm.push_back((((temp.ndm[size/2-1]+temp.ndm[size/2])/2)*0.4)+(temp.egz*0.6));
+}
+else
+{
+temp.galutm.push_back(((temp.ndm[size/2])*0.4)+(temp.egz*0.6));
+}
+lent.push_back(temp);
+temp.ndm.clear();
+temp.vdrk.clear();
+temp.galutv.clear();
+temp.galutm.clear();
+}
 }
 }
 cout<<left<<setw(10)<<"Vardas"<<setw(10)<<"Pavarde"<<setw(15)<<"Galutinis (Vid.)"<<" / "<<"Galutinis(Med.)"<<endl; // Isvedimas
@@ -101,7 +122,7 @@ for(auto i: lent)
 {
 for(auto v: i.galutv)
 for(auto m: i.galutm)
-cout<<left<<setfill(' ')<<setw(10)<<i.vardas<<setfill(' ')<<setw(10)<<i.pavarde<<setfill(' ')<<setw(15)<<right<<fixed<<setprecision(2)<<v<<setfill(' ')<<setw(15)<<right<<fixed<<setprecision(2)<<m<<endl;
+cout<<left<<setfill(' ')<<setw(10)<<i.vardas<<setfill(' ')<<setw(10)<<i.pavarde<<setfill(' ')<<setw(15)<<right<<fixed<<setprecision(2)<<v<<setfill(' ')<<setw(18)<<right<<fixed<<setprecision(2)<<m<<endl;
 }
 return 0;
 }
