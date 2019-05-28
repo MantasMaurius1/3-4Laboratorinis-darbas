c46f89e010df6ad8f186525e994ba9ad8142aa21
MantasMaurius1 <48581823+MantasMaurius1@users.noreply.github.com>
2019-05-28 16:12:41 +0300

    Update v0.5.cpp

--- v0.5.cpp
+++ v0.5.cpp
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <iomanip>
 #include <vector>
+#include <list>
 #include <algorithm>
 #include <string>
 #include <numeric>
@@ -23,7 +24,7 @@ struct studentas
 };
 
 double nd; char uzkl='t'; string budas; char A[9999];
-vector<studentas> lent; studentas temp;
+list<studentas> lent; studentas temp;
 
 void skaiciavimai(){
 sort(temp.ndm.begin(), temp.ndm.end());
